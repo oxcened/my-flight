228c66830a99d05a1b50e3838119786f302157ca
Alen Ajam <alen.ajam@live.com>
2023-06-13 22:45:35 +0200

    feat(api): add db_config

--- api/utils/db.php
+++ api/utils/db.php
@@ -1,8 +1,10 @@
 <?php
 
+require_once "db_config.php";
+
 # Don't want to leak errors on the client for security
 mysqli_report(MYSQLI_REPORT_OFF);
 
-$db = new mysqli("localhost", "root", "", "myflight");
+$db = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_NAME);
 
 return $db;
